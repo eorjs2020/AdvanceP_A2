4ce38d422b36fd60d1cb72e92ebdef7d91c59797
Mick <mickx.shular@gmail.com>
2020-08-04 21:35:28 -0400

    Clean up

--- AdvanceProgrammingA2/AdvanceProgrammingA2/main.cpp
+++ AdvanceProgrammingA2/AdvanceProgrammingA2/main.cpp
@@ -1,3 +1,6 @@
+//Michael Shular 101273089
+//Daekoen Lee 101076401
+
 #include<iostream>
 #include<string>
 #include<cstdlib>
@@ -10,166 +13,111 @@ using namespace std;
 class Fighter
 {
 private:
-	string fName;
-	int damage;
-	int structStrength;
-	
+	string m_pFighterName;
+	int m_pDamage;
+	int m_pStructStrength;
 public:
-	
-	Fighter(string name, int dmg, int stStrength)
-	{
-		
-		fName = name;
-		damage = dmg;
-		structStrength = stStrength;
-		
+	Fighter(string name, int dmg, int stStrength) {	
+		m_pFighterName = name;
+		m_pDamage = dmg;
+		m_pStructStrength = stStrength;
 	}
-	void reduceStructure(int damage) 
-	{
-		structStrength -= damage;
+	void reduceStructure(int damage) {
+		m_pStructStrength -= damage;
 	}
-	string getFName()
-	{
-		return fName;
-	}
-
-	int	getStructStrength()
-	{
-		return structStrength;
-	}
-
-	int getDamage()
-	{
-		return damage;
-	}
-	string getInfo() 
-	{
+	string getInfo() {
 		ostringstream ss;
-		ss << "Fighter name        : " << fName << "\n";
-		ss << "Structural strength : " << structStrength << "\n";
-		ss << "Damage              :" << damage << "\n" << "\n";
+		ss << "Fighter name        : " << m_pFighterName << "\n";
+		ss << "Structural strength : " << m_pStructStrength << "\n";
+		ss << "Damage              : " << m_pDamage << "\n" << "\n";
 		return ss.str();
 	}
-
-
-
+	string getFName() { return m_pFighterName; }
+	int	getStructStrength() { return m_pStructStrength; }
+	int getDamage() { return m_pDamage; }
 };
 
-class Carrier
-{
+class Carrier {
 private:
-	string name;
-	int maxBays;
-	int numFighters;
-	int tail;
-	int top;
-	Fighter* bayList[100];
+	string m_pName;
+	int m_pMaxBays,m_pNumFighters, m_pTail, m_pTop;
+	Fighter* m_pBayList[100];
 public:
-	Carrier(int max, string name)
-	{
-		this->name = name;
-		numFighters = 0;
-		tail = top = -1;
-		this->maxBays = max;
-		
+	Carrier(int max, string name) {
+		this->m_pName = name;
+		m_pNumFighters = 0;
+		m_pTail = m_pTop = -1;
+		this->m_pMaxBays = max;
 		//bayList[max];
 	}
-
-	bool loadFighter(Fighter* f)
-	{
-		if (numFighters == maxBays)
+	bool loadFighter(Fighter* f) {
+		if (m_pNumFighters == m_pMaxBays)
 			return false;
-		
-		if (top == -1)
-		{
-			top = tail = 0;
-			bayList[tail] = f;
+		if (m_pTop == -1) {
+			m_pTop = m_pTail = 0;
+			m_pBayList[m_pTail] = f;
 
 		}
-		else
-		{
-			tail = (tail + 1) % maxBays;
-			bayList[tail] = f;
+		else {
+			m_pTail = (m_pTail + 1) % m_pMaxBays;
+			m_pBayList[m_pTail] = f;
 		}
-		++numFighters;
+		++m_pNumFighters;
 		return true;
-		
 	}
-	Fighter* launchNextFighter()
-	{
-		
-	
+	Fighter* launchNextFighter(){
 		Fighter* curr = nullptr;
-		if (hasFighters())
-		{
-			curr = bayList[top];
-		
-			if (top == tail)
-			{
-				
-				top = tail = -1;
+		if (hasFighters()) {
+			curr = m_pBayList[m_pTop];
+			if (m_pTop == m_pTail) {
+				m_pTop = m_pTail = -1;
 			}
-			else
-			{
-				
-				
-				top = (top + 1) % maxBays;
-				
+			else{	
+				m_pTop = (m_pTop + 1) % m_pMaxBays;
 			}
-			--numFighters;
+			--m_pNumFighters;
 		}
-		else
-		{
+		else {
 			cout << "Empty" << endl;
-			
 		}
 		return curr;
 	}
-	string getInfo()
-	{
+	string getInfo() {
 		ostringstream ss;
-		ss << "Carrier name: " << name << "\n";
-		for (int i = 0; i < numFighters; i++)
-		{
-			ss << bayList[i]->getInfo();
+		ss << "Carrier name: " << m_pName << "\n";
+		for (int i = 0; i < m_pNumFighters; i++) {
+			ss << m_pBayList[i]->getInfo();
 		}
 		return ss.str();
 	}
-	bool hasFighters() {
-		
-		if (numFighters != 0)
+	bool hasFighters() {	
+		if (m_pNumFighters != 0)
 			return true;
-
 		return false;
 	}
-	
-	
-	int getCapacity() {	return maxBays;	}
-
-	int getNumFighters(){ return numFighters; }
-
-	string getName(){ return name; }
+	int getCapacity() {	return m_pMaxBays;	}
+	int getNumFighters(){ return m_pNumFighters; }
+	string getName(){ return m_pName; }
 };
 
-void battle(Carrier* a, Carrier* b)
-{
-	
+void battle(Carrier* a, Carrier* b) {
 	Fighter* tempf1, * tempf2, *first, *second;
 	Carrier* tempc1 = a, * tempc2 = b;
-	int roll, rollfirst, damageroll, turn;
-	turn = 0;
+	int rollToHit, rollToAttackFirst, damageRoll, amountOfTurns = 0;
 	
-	while (tempc1->hasFighters() && tempc2->hasFighters()) 
-	{
+	//Step 4 Loop for battle until no more ships
+	while (tempc1->hasFighters() && tempc2->hasFighters()) {
 		system("cls");
+		//Step 1 both carriers launch fighters 
 		tempf1 = tempc1->launchNextFighter();
 		tempf2 = tempc2->launchNextFighter();
-		while (tempf1->getStructStrength() > 0 && tempf2->getStructStrength() > 0)
-		{
-			roll = damageroll = rollfirst = 0;
-			cout << "Turn: " << turn << endl;
-			rollfirst = rand() % 2 + 1;
-			if(rollfirst){
+		//Step 2 loop for fighters to attack
+		while (tempf1->getStructStrength() > 0 && tempf2->getStructStrength() > 0) {
+			rollToHit = damageRoll = rollToAttackFirst = 0;
+			cout << "Turn: " << amountOfTurns << endl;
+			//Step 2.1 randomize turn order 
+			rollToAttackFirst = rand() % 2 + 1;
+			if(rollToAttackFirst){
 				first = tempf1;
 				second = tempf2;
 			}
@@ -177,30 +125,32 @@ void battle(Carrier* a, Carrier* b)
 				first = tempf2;
 				second = tempf1;
 			}
-			roll = rand() % 100 + 1;
-			if (roll >= 50) {		
-				damageroll = rand() % first->getDamage() + 1;
-				second->reduceStructure(damageroll);
-				cout << first->getFName() << " hits " << second->getFName() << " for " << damageroll << endl;
+			//Step 2.2 Check to see if attack hits or misses
+			rollToHit = rand() % 100 + 1;
+			if (rollToHit >= 50) {		
+				damageRoll = rand() % first->getDamage() + 1;
+				second->reduceStructure(damageRoll);
+				cout << first->getFName() << " hits " << second->getFName() << " for " << damageRoll << endl;
 			}
 			else {
 				cout << first->getFName() << " misses " << second->getFName() << endl;
 			}
-			roll = rand() % 100 + 1;
+			//Step 2.4 Oppsite fighter attacks 
+			rollToHit = rand() % 100 + 1;
 			if (second->getStructStrength() <= 0 ){
 				break;
 			}
-			if (roll >= 50) {	
-				damageroll = rand() % second->getDamage() + 1;
-				first->reduceStructure(damageroll);
-				cout << second->getFName() << " hits " << first->getFName() << " for " << damageroll << endl;
+			if (rollToHit >= 50) {	
+				damageRoll = rand() % second->getDamage() + 1;
+				first->reduceStructure(damageRoll);
+				cout << second->getFName() << " hits " << first->getFName() << " for " << damageRoll << endl;
 			}
 			else {
 				cout << second->getFName() << " misses " << first->getFName() << endl;
 			}
-			++turn;
+			++amountOfTurns;
 		}
-		
+		//Step 3 Loading suviving fighter
 		if (tempf1->getStructStrength() > 0){
 			tempc1->loadFighter(tempf1);
 		}
@@ -223,59 +173,57 @@ void battle(Carrier* a, Carrier* b)
 	}
 }
 
-
-int main()
-{
-	srand((unsigned)time(0));
+int main() {
 	Carrier* c1,* c2;
-	Fighter* ftemp;
+	Fighter* tempFighter;
 	std::ifstream in;
-	int m, n, n1;
-	std::string name;
+	int bayCapacity, maxFighters, structStrength, damage;
+	std::string nameCarrier, nameFighter;
+	
+	srand((unsigned)time(0));
+	//Step 1. Loading data for text files
 	in.open("shipData.txt");
 	if (!in.is_open()) {
 		std::cout << "Error";
 	}
-	
-	in >> name;
+	in >> nameCarrier;
 	in.ignore();
-	in >> m;
-	//in.ignore();
-	in >> n; 
-	c1 = new Carrier(m, name);
-	for (int x = 0; x < n; ++x) {
-		in >> name;
+	in >> bayCapacity;
+	in >> maxFighters; 
+	c1 = new Carrier(bayCapacity, nameCarrier);
+	for (int x = 0; x < maxFighters; ++x) {
+		in >> nameFighter;
 		in.ignore();
-		in >> m;
-			
-		in >> n1;
-		ftemp = new Fighter(name, n1, m);
-		c1->loadFighter(ftemp);
+		in >> damage;
+		in >> structStrength;
+		tempFighter = new Fighter(nameFighter, structStrength, damage);
+		c1->loadFighter(tempFighter);
 	}
-	in >> name;
+	in >> nameCarrier;
 	in.ignore();
-	in >> m;
-
-	in >> n;
-	c2 = new Carrier(m, name);
-	for (int x = 0; x < n; ++x) {
-		in >> name;
+	in >> bayCapacity;
+	in >> maxFighters;
+	c2 = new Carrier(bayCapacity, nameCarrier);
+	for (int x = 0; x < maxFighters; ++x) {
+		in >> nameFighter;
 		in.ignore();
-		in >> m;
-			
-		in >> n1;
-		ftemp = new Fighter(name, n1, m);
-		c2->loadFighter(ftemp);
+		in >> damage;
+		in >> structStrength;
+		tempFighter = new Fighter(nameFighter, structStrength, damage);
+		c2->loadFighter(tempFighter);
 	}
-	
 	in.close();
-	
-
+	//Step 2. Print out carrier data 
 	cout << c1->getInfo();
 	cout << c2->getInfo();
 	system("pause");
+	//Step 3. Battle simulation 
 	battle(c1, c2);
-
+	system("pause");
+	//Step 4. Print out carrier data
+	system("cls");
+	cout << c1->getInfo() << endl;
+	cout << c2->getInfo();
 	system("pause");
 	return 0;
 }
